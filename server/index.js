f9f220c760f4f1da321d41db8000a2764b4e836b
taisan0816 <guruyo7835@gmail.com>
2025-01-08 14:23:30 +0900

    Update gitignore and success connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const PORT = 5000;
+require("dotenv").config()
 
-app.get("/", (req,res) => {
-    res.send("Hello Express")
-})
+//DB接続
+try {
+    mongoose.connect(process.env.MONGODB_URL);
+    console.log("DBと接続中...")
+} catch(error) {
+    console.log(error)
+}
+
+//ユーザー新規登録API
+
+//ユーザーログイン用API
+
 
 app.listen(PORT, () => {
     console.log('ローカルサーバー起動中')
