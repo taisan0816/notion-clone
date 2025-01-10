82d962f8e5881c3c313393927883b430175f2d27
taisan0816 <guruyo7835@gmail.com>
2025-01-10 22:10:49 +0900

    feat(feature/2):ログインと新規登録コンポーネント作成

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import AuthLayout from './components/layout/AuthLayout';
+import Login from './pages/Login';
+import Register from './pages/Register'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {createTheme, ThemeProvider} from "@mui/material/styles"
+import { dark } from '@mui/material/styles/createPalette';
+import { CssBaseline } from '@mui/material';
+import { blue } from '@mui/material/colors';
+
+function App() {
+
+  const theme = createTheme ({
+    palette: {
+      primary: blue
+    }
+  })
+  return (
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />} >
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
